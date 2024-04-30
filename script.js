ddd78c6b12f5cf0d54ae81f1daac4a9f64b85506
Praveen Tiwari <100703294+PraveenTiwari08@users.noreply.github.com>
2024-04-30 23:50:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 function firstWord(s) {
   // your code here
+	 str = ""
 	for(let i = 0 ; i < s.length ; i++){
 		if(s[i] === ""){
 			break;
 		}
-		return s[i];
+		 str += s[i];
 	}
+	return str;
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
-alert(firstWord(s));
+// const s = prompt("Enter String:");
+// alert(firstWord(s));
